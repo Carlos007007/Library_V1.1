2d0e1e1b651488c58b15e2ebc3e7a1b59c040e3e
Carlos Alfaro <carlosalfaro.info@gmail.com>
2024-09-24 16:19:00 -0600

    Update footer.php

--- inc/footer.php
+++ inc/footer.php
@@ -1,23 +1,15 @@
 <footer class="footer full-reset">
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-xs-12 col-sm-6">
-                <h4 class="all-tittles">Acerca de</h4>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos a voluptatibus quae odio. Fugiat omnis dolores totam quos debitis ratione corrupti! Repudiandae quia sapiente repellendus ab consequatur eum blanditiis dolorum.
-                </p>
-            </div>
-            <div class="col-xs-12 col-sm-6">
-                <h4 class="all-tittles">Desarrollador</h4>
-                <ul class="list-unstyled">
-                    <li>
-                        <i class="zmdi zmdi-check zmdi-hc-fw"></i>&nbsp; Carlos Alfaro 
-                        <i class="zmdi zmdi-facebook zmdi-hc-fw footer-social" data-link="https://www.facebook.com/CarlosAlfaroES/"></i>
-                        <i data-link="https://www.youtube.com/c/CarlosAlfaro007" class="zmdi zmdi-youtube zmdi-hc-fw footer-social"></i>
-                    </li>
-                </ul>
-            </div>
-        </div>
+    <div class="container-fluid lead">
+        La versión 1.4 de este sistema está disponible para los <a href="https://www.youtube.com/channel/UCRMJ0vxtnHh_UAq1Yx9BYWQ/join" target="_blank" style="color: #FF9900;">MIEMBROS del canal de YouTube (Membresía "Programador Semi Senior" o superior)</a>, puede hacerse miembro <a href="https://www.youtube.com/channel/UCRMJ0vxtnHh_UAq1Yx9BYWQ/join" target="_blank" style="color: #FF9900;">haciendo clic acá</a> o probar la DEMO de la versión 1.4 <a href="http://systems.designlopers.com/SRCBV14/" target="_blank" style="color: #FF9900;">haciendo clic aquí</a>.
+        <br><br>
+        <h4 class="all-tittles">Desarrollador</h4>
+        <ul class="list-unstyled">
+            <li>
+                <i class="zmdi zmdi-check zmdi-hc-fw"></i>&nbsp; Carlos Alfaro 
+                <i class="zmdi zmdi-facebook zmdi-hc-fw footer-social" data-link="https://www.facebook.com/CarlosAlfaroES/"></i>
+                <i data-link="https://www.youtube.com/c/CarlosAlfaro007" class="zmdi zmdi-youtube zmdi-hc-fw footer-social"></i>
+            </li>
+        </ul>
     </div>
-    <div class="footer-copyright full-reset all-tittles">© 2017 Carlos Alfaro todos los derechos reservados</div>
+    <div class="footer-copyright full-reset all-tittles">&copy; <?php echo date("Y"); ?> Carlos Alfaro todos los derechos reservados</div>
 </footer>
